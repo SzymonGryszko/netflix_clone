282d8c2802ca4ff01abeafc96f4a05d9cb174bf4
Szymon Gryszko <gryszkoszymon@gmail.com>
2021-04-25 21:51:55 +0200

    App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/screens/screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Netflix UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: Colors.black),
+      home: HomeScreen(),
     );
   }
 }
