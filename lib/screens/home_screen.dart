8422a206c00e14442d0fcb3e1a00ce72f7b9f217
Szymon Gryszko <gryszkoszymon@gmail.com>
2021-04-27 19:15:51 +0200

    content list scrolls

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -57,7 +57,29 @@ class _HomeScreenState extends State<HomeScreen> {
             sliver: SliverToBoxAdapter(
               child: Previews(title: 'Previews', contentList: previews),
             ),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title: 'My List',
+              contentList: myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title: 'Netflix Originals',
+              contentList: originals,
+              isOriginals: true,
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(bottom: 20,0),
+                      sliver: SliverToBoxAdapter(
+              child: ContentList(
+                title: 'Trending',
+                contentList: trending,
+              ),
+            ),
+          ),
         ],
       ),
     );
